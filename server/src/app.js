7fb7c48df9b79a58c9dcfc3c22fa30a809e6b63d
sachinmags5 <sachinmags5@gmail.com>
2026-03-13 12:43:57 +0530

    cors bug fix

--- server/src/app.js
+++ server/src/app.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(
   cors({
     // origin: "http://localhost:5173", // frontend URL
-    origin: ["http://localhost:5173", "https://takecutfilms.vercel.app/"],
+    origin: ["http://localhost:5173", "https://takecutfilms.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   }),
